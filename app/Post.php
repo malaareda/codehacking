1a8ffd7d17fd0a2a0e974092d63d2b6b7b0a5c93
malaareda <mohamed.alaa.reda@gmail.com>
2017-03-14 22:27:32 +0200

    my final project

--- app/Post.php
+++ app/Post.php
@@ -2,18 +2,36 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     //
 
+    use Sluggable;
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
     protected $fillable = [
 
         'category_id',
         'photo_id',
         'title',
-        'body'
+        'body',
+        'slug'
     ];
 
 
@@ -27,10 +45,28 @@ class Post extends Model
         return $this->belongsTo('App\Photo');
     }
 
+    public function slug(){
+
+        return $this->belongsTo('App/Sluggable');
+    }
+
     public function category(){
 
         return $this->belongsTo('App\Category');
     }
+
+//    public function comments(){
+//
+//        return $this->hasMany('App\Post');
+//    }
+
+
+    public function comments(){
+
+        return $this->hasMany('App\Comment');
+
+    } /* End of public function comments() */
+
 
 
 
